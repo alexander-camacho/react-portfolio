d41e6e76995840acf0a80dbbee7cf3fb17644339
Alex Camacho <alsbrain@optonline.net>
2021-03-13 14:36:58 -0800

    updated a tags to Link from react-router-dom

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,4 +1,5 @@
 import React from "react"
+import {Link } from "react-router-dom"
 
 
 function Header() {
@@ -7,22 +8,22 @@ function Header() {
         <nav className="navbar navbar-expand-md navbar-light bg-info">
             <div className="container">
 
-                <a className="navbar-brand" href="/">
+                <Link className="navbar-brand" to="/">
                     Alexander Camacho
-        </a>
+        </Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarNav">
                     <ul className="navbar-nav">
                         <li className="nav-item">
-                            <a className="nav-link" href="/about">About</a>
+                            <Link className="nav-link" to="/about">About</Link>
                         </li>
                         <li>
-                            <a className="nav-link" href="/contact">Contact</a>
+                            <Link className="nav-link" to="/contact">Contact</Link>
                         </li>
                         <li>
-                            <a className="nav-link" href="/projects">Projects</a>
+                            <Link className="nav-link" to="/projects">Projects</Link>
                         </li>
                     </ul>
                 </div>
