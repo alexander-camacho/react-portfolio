3bf16c03dc5ae60f4644e577ba5e5e2ffad9b333
Alex Camacho <alsbrain@optonline.net>
2021-03-13 14:07:50 -0800

    updated footer to include links

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,12 +1,22 @@
 import React from "react"
-
-
+import "./style.css"
 const Footer = () => {
     return (
-        <footer>
-            <p>I'm a Footer!</p>
+            <footer >
+                <div className="container">
 
-        </footer>
+                <h2>Check out my other work</h2>
+                <div className="row">
+                    <div className="LI-profile-badge col-6" data-version="v1" data-size="medium" data-locale="en_US" data-type="horizontal" data-theme="light" data-vanity="alexander-camacho-940427150"><a className="LI-simple-link" href='https://www.linkedin.com/in/alexander-camacho-940427150?trk=profile-badge'>Alexander Camacho</a></div>
+
+                    <div className="col-6">
+
+                    <a href="https://github.com/alexander-camacho">Github</a>
+                    </div>
+                </div>
+                </div>
+
+            </footer>
     )
 }
 
