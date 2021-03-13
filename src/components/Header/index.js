852a5076159081812e9ec2d89fe973671facff7d
Alex Camacho <alsbrain@optonline.net>
2021-03-13 14:08:18 -0800

    header styling

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,27 +4,30 @@ import React from "react"
 function Header() {
 
     return (
-        <nav className="navbar navbar-expand-md navbar-dark bg-primary">
-        <a className="navbar-brand" href="/">
-            Alexander Camacho
+        <nav className="navbar navbar-expand-md navbar-light bg-info">
+            <div className="container">
+
+                <a className="navbar-brand" href="/">
+                    Alexander Camacho
         </a>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav">
-                <li className="nav-item">
-                    <a className="nav-link" href="/about">About</a>
-                </li>
-                <li>
-                    <a className="nav-link" href="/home">Home</a>
-                </li>
-                <li>
-                    <a className="nav-link" href="/projects">Projects</a>
-                </li>
-            </ul>
-        </div>
-    </nav >
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav">
+                        <li className="nav-item">
+                            <a className="nav-link" href="/about">About</a>
+                        </li>
+                        <li>
+                            <a className="nav-link" href="/contact">Contact</a>
+                        </li>
+                        <li>
+                            <a className="nav-link" href="/projects">Projects</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav >
     )
 }
 
