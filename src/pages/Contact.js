c13944ae18f661d745c580d93b488f93b84374b6
Alex Camacho <alsbrain@optonline.net>
2021-03-13 14:19:07 -0800

    updated syntax

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -3,24 +3,26 @@ import React from "react"
 
 const Contact = () => {
     return (
-        <div>
-            <form class="container-sm">
+        <div className="container">
+            <form className="container-sm">
                 <h1>Leave a message</h1>
-                <div class="form-group">
-                    <label for="exampleInputName">Name</label>
-                    <input type="text" class="form-control" id="exampleInputName" placeholder="Name" />
+                <div className="form-group">
+                    <label htmlFor="exampleInputName">Name</label>
+                    <input type="text" className="form-control" id="exampleInputName" placeholder="Name" />
                 </div>
-                <div class="form-group">
-                    <label for="exampleInputEmail1">Email address</label>
-                    <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Email" />
+                <div className="form-group">
+                    <label htmlFor="exampleInputEmail1">Email address</label>
+                    <input type="email" className="form-control" id="exampleInputEmail1" placeholder="Email" />
                 </div>
-                <div class="form-group">
-                    <label for="exampleFormControlTextarea1">Leave a message!</label>
-                    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" placeholder="Leave a message"></textarea>
+                <div className="form-group">
+                    <label htmlFor="exampleFormControlTextarea1">Leave a message!</label>
+                    <textarea className="form-control" id="exampleFormControlTextarea1" rows="3" placeholder="Leave a message"></textarea>
                 </div>
 
-                <button type="submit" class="btn btn-primary">Submit</button>
+                <button type="submit" className="btn btn-primary disabled">Submit</button>
             </form>
+
+            <p ><b>Email me at:</b> alsbrain@optonline.net</p>
         </div>
     )
 }
